f8d57dcbbff7ddc9b7a49f2d2e036ffe2d46388b
Jonathan Tripathy <jt@puppetlabs.com>
2015-10-03 15:40:09 +0100

    Added syslog_setting

---
+++ tests/test_syslog_settings.rb
@@ -0,0 +1,54 @@
+#
+# Minitest for SyslogSetting class
+#
+# Copyright (c) 2014-2015 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require File.expand_path('../ciscotest', __FILE__)
+require File.expand_path('../../lib/cisco_node_utils/syslog_settings', __FILE__)
+
+# TestSyslogSetting - Minitest for SyslogSetting node utility.
+class TestSyslogSettings < CiscoTestCase
+  def setup
+    # setup runs at the beginning of each test
+    super
+    no_syslogsettings
+  end
+
+  def teardown
+    # teardown runs at the end of each test
+    no_syslogsettings
+    super
+  end
+
+  def no_syslogsettings
+    # Turn the feature off for a clean test.
+    config('no logging timestamp seconds')
+  end
+
+  # TESTS
+
+  def test_syslogsettings_create
+    syslog_setting = Cisco::SyslogSettings.new('default')
+    assert_includes(Cisco::SyslogSettings.syslogsettings, 'default')
+    assert_equal(Cisco::SyslogSettings.syslogsettings['default'],
+                 syslog_setting)
+
+    syslog_setting.timestamp = 'milliseconds'
+    assert_equal(Cisco::SyslogSettings.syslogsettings['default'].timestamp,
+                 'milliseconds')
+    assert_equal(syslog_setting.timestamp,
+                 'milliseconds')
+  end
+end
