201f1b27b713a70a4282ae0cdf2ca94a722f021b
Chris Van Heuveln <cvanheuv@cisco.com>
2015-11-13 09:28:47 -0500

    speed_change_disallowed missed one error message

--- tests/test_interface.rb
+++ tests/test_interface.rb
@@ -78,7 +78,8 @@ class TestInterface < CiscoTestCase
   # Helper to check for misc speed change disallowed error messages.
   def speed_change_disallowed(message)
     pattern = Regexp.new('(port doesn t support this speed|' \
-                         'Changing interface speed is not permitted)')
+                         'Changing interface speed is not permitted|' \
+                         'requested config change not allowed)')
     skip('Skip test: Interface type does not allow config change') if
          message[pattern]
     flunk(message)
