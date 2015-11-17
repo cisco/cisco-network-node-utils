0cf4d3d9b806ce636850f571d178606906f18916
mikewiebe <mwiebe@cisco.com>
2015-11-17 11:45:17 -0500

    vxlan_vtep updates

--- lib/cisco_node_utils/vxlan_vtep.rb
+++ lib/cisco_node_utils/vxlan_vtep.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require File.join(File.dirname(__FILE__), 'node_util')
+require_relative 'node_util'
 
 module Cisco
   # node_utils class for vxlan_vtep
@@ -35,7 +35,7 @@ module Cisco
     def self.vteps
       hash = {}
       is_vxlan_feature = config_get('vxlan', 'feature')
-      return hash if (:enabled != is_vxlan_feature.first.to_sym)
+      return hash if (:enabled != is_vxlan_feature.to_sym)
       vtep_list = config_get('vxlan_vtep', 'all_interfaces')
       return hash if vtep_list.nil?
 
@@ -50,7 +50,7 @@ module Cisco
       vxlan = config_get('vxlan', 'feature')
       fail 'vxlan/nv_overlay feature not found' if vxlan.nil?
       return :disabled if vxlan.nil?
-      vxlan.first.to_sym
+      vxlan.to_sym
     end
 
     def vxlan_feature_set(vxlan_set)
@@ -71,9 +71,12 @@ module Cisco
     def create
       unless (:enabled == vxlan_feature)
         vdc_name = config_get('limit_resource', 'vdc')
-        @apply_to = vdc_name.first
-        debug("###### VDC is #{@apply_to}")
-        config_set('limit_resource', 'vxlan', @apply_to)
+        # Only supported on n7k currently.
+        unless vdc_name.nil?
+          @apply_to = vdc_name
+          debug("###### VDC is #{@apply_to}")
+          config_set('limit_resource', 'vxlan', @apply_to)
+        end
         vxlan_feature_set(:enabled)
       end
       # re-use the "interface command ref hooks"
@@ -85,14 +88,18 @@ module Cisco
       config_set('interface', 'destroy', @name)
     end
 
+    def ==(other)
+      name == other.name
+    end
+
     ########################################################
     #                      PROPERTIES                      #
     ########################################################
 
     def description
       desc = config_get('interface', 'description', @name)
-      return '' if desc.nil?
-      desc.shift.strip
+      return default_description if desc.nil?
+      desc
     end
 
     def description=(desc)
@@ -110,72 +117,76 @@ module Cisco
       config_get_default('interface', 'description')
     end
 
-    def vni_mcast_grp_map
-      final_hash = {}
-      show = show("show running-config interface #{@name}")
-      debug("show class is #{show.class} and show op is #{show}")
-      return final_hash if show == {}
-      match_pat1 = /vni\s+(\S+).*mcast\-group\s+(\S+)/m
-      match_pat2 = /vni\s+(\S+).*(associate\-vrf)/m
-      split_pat = /member\s+/
-      pair_arr = show.split(split_pat)
-      pair_arr.each do |pair|
-        match_arr = match_pat1.match(pair)
-        match_arr ||= match_pat2.match(pair)
-        unless match_arr.nil?
-          debug "match arr 1 : #{match_arr[1]} 2: #{match_arr[2]}"
-          final_hash[match_arr[1]] = "#{match_arr[2]}"
-        end
-      end
-      final_hash
-    end
+    # TODO: Move vni_mcast_grp_map to vxlan_vtp_vni.rb object
+    #
+    # def vni_mcast_grp_map
+    #  final_hash = {}
+    #  show = show("show running-config interface #{@name}")
+    #  debug("show class is #{show.class} and show op is #{show}")
+    #  return final_hash if show == {}
+    #  match_pat1 = /vni\s+(\S+).*mcast\-group\s+(\S+)/m
+    #  match_pat2 = /vni\s+(\S+).*(associate\-vrf)/m
+    #  split_pat = /member\s+/
+    #  pair_arr = show.split(split_pat)
+    #  pair_arr.each do |pair|
+    #    match_arr = match_pat1.match(pair)
+    #    match_arr ||= match_pat2.match(pair)
+    #    unless match_arr.nil?
+    #      debug "match arr 1 : #{match_arr[1]} 2: #{match_arr[2]}"
+    #      final_hash[match_arr[1]] = "#{match_arr[2]}"
+    #    end
+    #  end
+    #  final_hash
+    # end
 
-    def vni_mcast_grp_map=(val, prev_val=nil)
-      debug "val is of class #{val.class} and is #{val} prev is #{prev_val}"
-      # When prev_val is nil, HashDiff doesn't do a `+' on each element, so this
-      debug "value of mac_dist = #{@mac_dist_proto}"
-      param_hash = {}
-      # supress_arp = (@mac_dist_proto == :evpn) ? "supress-arp" : ""
-      if prev_val.nil?
-        val.each do |fresh_vni, fresh_mgrp|
-          param_hash = { name: @name, no_cmd: '', vni: fresh_vni,
-                         mcast_grp: fresh_mgrp }
-          config_set('vxlan_vtep', 'member_vni_mgrp', param_hash)
-        end
-        return
-      end
-      require 'hashdiff'
-      hash_diff = HashDiff.diff(prev_val, val)
-      debug "hsh diff ; #{hash_diff}"
-      return if hash_diff == []
-      hash_diff.each do |diff|
-        case diff[0]
-        when /\+/
-          param_hash = { name: @name, no_cmd: '', vni: diff[1],
-                         mcast_grp: diff[2] }
-        when /\-/
-          param_hash = { name: @name, no_cmd: 'no', vni: diff[1],
-                         mcast_grp: diff[2] }
-        when /~/
-          param_hash = { name: @name, no_cmd: 'no', vni: diff[1],
-                         mcast_grp: diff[2] }
-          config_set('vxlan_vtep', 'member_vni_mgrp', param_hash)
-          param_hash = { name: @name, no_cmd: '', vni: diff[1],
-                         mcast_grp: diff[3] }
-        end
-        config_set('vxlan_vtep', 'member_vni_mgrp', param_hash)
-      end
-    rescue CliError => e
-      raise "[vxlan_vtep #{@name}] '#{e.command}' : #{e.clierror}"
-    end
+    # TODO: Move vni_mcast_grp_map to vxlan_vtp_vni.rb object
+    #
+    # def vni_mcast_grp_map=(val, prev_val=nil)
+    #  debug "val is of class #{val.class} and is #{val} prev is #{prev_val}"
+    #  # When prev_val is nil, HashDiff doesn't do a `+' on each element, so
+    #  # this
+    #  debug "value of mac_dist = #{@mac_dist_proto}"
+    #  param_hash = {}
+    #  # supress_arp = (@mac_dist_proto == :evpn) ? "supress-arp" : ""
+    #  if prev_val.nil?
+    #    val.each do |fresh_vni, fresh_mgrp|
+    #      param_hash = { name: @name, no_cmd: '', vni: fresh_vni,
+    #                     mcast_grp: fresh_mgrp }
+    #      config_set('vxlan_vtep', 'member_vni_mgrp', param_hash)
+    #    end
+    #    return
+    #  end
+    #  require 'hashdiff'
+    #  hash_diff = HashDiff.diff(prev_val, val)
+    #  debug "hsh diff ; #{hash_diff}"
+    #  return if hash_diff == []
+    #  hash_diff.each do |diff|
+    #    case diff[0]
+    #    when /\+/
+    #      param_hash = { name: @name, no_cmd: '', vni: diff[1],
+    #                     mcast_grp: diff[2] }
+    #    when /\-/
+    #      param_hash = { name: @name, no_cmd: 'no', vni: diff[1],
+    #                     mcast_grp: diff[2] }
+    #    when /~/
+    #      param_hash = { name: @name, no_cmd: 'no', vni: diff[1],
+    #                     mcast_grp: diff[2] }
+    #      config_set('vxlan_vtep', 'member_vni_mgrp', param_hash)
+    #      param_hash = { name: @name, no_cmd: '', vni: diff[1],
+    #                     mcast_grp: diff[3] }
+    #    end
+    #    config_set('vxlan_vtep', 'member_vni_mgrp', param_hash)
+    #  end
+    # rescue CliError => e
+    #  raise "[vxlan_vtep #{@name}] '#{e.command}' : #{e.clierror}"
+    # end
 
     def mac_distribution
       mac_dist = config_get('vxlan_vtep', 'mac_distribution', name: @name)
-      debug "mac_dist is #{mac_dist}"
       if mac_dist.nil?
         @mac_dist_proto = :flood
       else
-        @mac_dist_proto = (mac_dist.first.strip == 'bgp') ? :evpn : :flood
+        @mac_dist_proto = (mac_dist == 'bgp') ? :evpn : :flood
       end
       @mac_dist_proto.to_s
     end
@@ -200,8 +211,8 @@ module Cisco
     def source_interface
       src_intf = config_get('vxlan_vtep', 'source_intf', name: @name)
       debug "src_intf is #{src_intf}"
-      return '' if src_intf.nil?
-      src_intf.first.strip
+      return default_source_interface if src_intf.nil?
+      src_intf
     end
 
     def source_interface=(val)
@@ -215,6 +226,10 @@ module Cisco
       end
     rescue Cisco::CliError => e
       raise "[#{@name}] '#{e.command}' : #{e.clierror}"
+    end
+
+    def default_source_interface
+      config_get_default('vxlan_vtep', 'source_intf')
     end
 
     def shutdown
---
+++ tests/test_vxlan_vtep.rb
@@ -0,0 +1,160 @@
+# Copyright (c) 2013-2015 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative 'ciscotest'
+require_relative '../lib/cisco_node_utils/vxlan_vtep'
+
+include Cisco
+
+# TestVxlanGlobal - Minitest for VxlanGlobal node utility
+class TestVxlanVtep < CiscoTestCase
+  def setup
+    super
+    no_vxlan_global
+  end
+
+  def teardown
+    no_vxlan_global
+    super
+  end
+
+  def no_vxlan_global
+    config('no feature nv overlay')
+  end
+
+  def test_create_destroy_one
+    id = 'nve1'
+    vtep = VxlanVtep.new(id)
+    @default_show_command = "show running | i 'interface #{id}'"
+
+    assert_show_match(pattern: /^interface #{id}$/,
+                      msg:     "failed to create interface #{id}")
+
+    assert_includes(Cisco::VxlanVtep.vteps, id)
+    assert_equal(Cisco::VxlanVtep.vteps[id], vtep)
+
+    vtep.destroy
+    refute_show_match(pattern: /^interface #{id}$/,
+                      msg:     "failed to destroy interface #{id}")
+  end
+
+  def test_create_destroy_multiple
+    skip('Only supported on n7k') if node.product_id =~ /N[3|5|9]K/
+
+    id1 = 'nve1'
+    id2 = 'nve2'
+    id3 = 'nve3'
+    id4 = 'nve4'
+    vtep1 = VxlanVtep.new(id1)
+    vtep2 = VxlanVtep.new(id2)
+    vtep3 = VxlanVtep.new(id3)
+    vtep4 = VxlanVtep.new(id4)
+
+    [id1, id2, id3, id4].each do |id|
+      assert_show_match(command: "show running | i 'interface #{id}'",
+                        pattern: /^interface #{id}$/,
+                        msg:     "failed to create interface #{id}")
+      assert_includes(Cisco::VxlanVtep.vteps, id)
+    end
+
+    vtep1.destroy
+    vtep2.destroy
+    vtep3.destroy
+    vtep4.destroy
+
+    [id1, id2, id3, id4].each do |id|
+      refute_show_match(command: "show running | i 'interface #{id}'",
+                        pattern: /^interface #{id}$/,
+                        msg:     "failed to create interface #{id}")
+    end
+  end
+
+  def test_create_multiple_negative
+    # N[3|9]K supports a single nve int, N7K supports 4.
+    case node.product_id
+    when /N[3|9]K/
+      VxlanVtep.new('nve1')
+      negative_id = 'nve2'
+    when /N7K/
+      (1..4).each { |n| VxlanVtep.new("nve#{n}") }
+      negative_id = 'nve5'
+    end
+
+    assert_raises(CliError) do
+      VxlanVtep.new(negative_id)
+    end
+  end
+
+  def test_description
+    id = 'nve1'
+    vtep = VxlanVtep.new(id)
+
+    # Set description to non-default value and verify
+    desc = 'vxlan interface'
+    vtep.description = desc
+    assert_equal(vtep.description, desc, "description is not #{desc}")
+
+    # Set description to default value and verfiy
+    desc = vtep.default_description
+    vtep.description = desc
+    assert_equal(vtep.description, desc, "description is not #{desc}")
+  end
+
+  def test_mac_distribution
+    id = 'nve1'
+    vtep = VxlanVtep.new(id)
+
+    val = :flood
+    vtep.mac_distribution = val
+    assert_equal(vtep.mac_distribution, val.to_s,
+                 "mac_distribution is not #{val}")
+
+    val = :evpn
+    vtep.mac_distribution = val
+    assert_equal(vtep.mac_distribution, val.to_s,
+                 "mac_distribution is not #{val}")
+
+    # Set value back to flood, currently evpn.
+    val = :flood
+    vtep.mac_distribution = val
+    assert_equal(vtep.mac_distribution, val.to_s,
+                 "mac_distribution is not #{val}")
+  end
+
+  def test_shutdown
+    id = 'nve1'
+    vtep = VxlanVtep.new(id)
+
+    vtep.shutdown = true
+    assert(vtep.shutdown, 'source_interface is not shutdown')
+
+    vtep.shutdown = false
+    refute(vtep.shutdown, 'source_interface is shutdown')
+  end
+
+  def test_source_interface
+    id = 'nve1'
+    vtep = VxlanVtep.new(id)
+
+    # Set source_interface to non-default value
+    val = 'loopback55'
+    vtep.source_interface = val
+    assert_equal(vtep.source_interface, val, "source_interface is not #{val}")
+
+    # Set source_interface to default value
+    val = vtep.default_source_interface
+    vtep.source_interface = val
+    assert_equal(vtep.source_interface, val, "source_interface is not #{val}")
+  end
+end
