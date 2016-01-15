2f23b7fb8bc5626f762432e615797612d389d2d4
Glenn Matthews <glmatthe@cisco.com>
2016-01-15 17:03:35 -0500

    Cleanup some TODO items

--- spec/yaml_spec.rb
+++ spec/yaml_spec.rb
@@ -4,12 +4,6 @@ require 'yaml'
 
 files = Dir.glob(__dir__ + '/../lib/cisco_node_utils/cmd_ref/*.yaml')
 
-# TODO: use Kwalify::MetaValidator to validate the schema itself
-
-schema = Kwalify::Yaml.load_file(File.join(__dir__, 'schema.yaml'))
-validator = Kwalify::Validator.new(schema)
-parser = Kwalify::Yaml::Parser.new(validator)
-
 def print_errors(errors)
   str = "Schema validation errors:\n"
   error_str_list = errors.map do |e|
@@ -17,6 +11,25 @@ def print_errors(errors)
   end
   str + error_str_list.join("\n")
 end
+
+# Use the MetaValidator to make sure the schema itself is sane
+metavalidator = Kwalify::MetaValidator.instance
+
+schema_file = File.join(__dir__, 'schema.yaml')
+
+metaparser = Kwalify::Yaml::Parser.new(metavalidator)
+context 'schema.yaml' do
+  it 'should have no schema metavalidation errors' do
+    metaparser.parse_file(schema_file)
+    errors = metaparser.errors()
+    expect(errors).to be_empty, -> { print_errors(errors) }
+  end
+end
+
+# Then use the Validator to make sure our files comply with the schema
+schema = Kwalify::Yaml.load_file(schema_file)
+validator = Kwalify::Validator.new(schema)
+parser = Kwalify::Yaml::Parser.new(validator)
 
 files.each do |file|
   context file.split('/')[-1] do
