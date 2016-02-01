51bf1e1356e743de763d0faac1b0cf4d20972b34
Rob Gries <robgrie@cisco.com>
2016-02-01 10:44:33 -0500

    Change skip message and make helper method

--- tests/test_interface_switchport.rb
+++ tests/test_interface_switchport.rb
@@ -49,6 +49,11 @@ class TestInterfaceSwitchport < CiscoTestCase
     end
   end
 
+  def platform_supports_vtp_switchport_access?
+    skip('Platform does not support VTP when switchport mode is access') if
+      node.product_id =~ /N(5|6|7)/
+  end
+
   def system_default_switchport(state='')
     config("#{state} system default switchport")
   end
@@ -100,8 +105,7 @@ class TestInterfaceSwitchport < CiscoTestCase
   end
 
   def test_switchport_vtp_enabled_access
-    skip('Cannot apply VTP config because port mode is access') if
-      node.product_id =~ /N(5|6|7)/
+    platform_supports_vtp_switchport_access?
     vtp = Vtp.new(true)
     interface = Interface.new(interfaces[0])
     interface.switchport_mode = :access
@@ -149,8 +153,7 @@ class TestInterfaceSwitchport < CiscoTestCase
   end
 
   def test_set_switchport_vtp_default_access
-    skip('Cannot apply VTP config because port mode is access') if
-      node.product_id =~ /N(5|6|7)/
+    platform_supports_vtp_switchport_access?
     vtp = Vtp.new(true)
     interface = Interface.new(interfaces[0])
     interface.switchport_mode = :access
@@ -192,8 +195,7 @@ class TestInterfaceSwitchport < CiscoTestCase
   end
 
   def test_set_switchport_vtp_true_access
-    skip('Cannot apply VTP config because port mode is access') if
-      node.product_id =~ /N(5|6|7)/
+    platform_supports_vtp_switchport_access?
     vtp = Vtp.new(true)
     interface = Interface.new(interfaces[0])
 
