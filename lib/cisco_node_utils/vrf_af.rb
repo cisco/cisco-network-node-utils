22e2bf174ab49cbcc772c1b4d207e0b520dca0fe
Helen Zhang <helzhang@cisco.com>
2016-02-16 14:33:27 -0500

    US63946 - add "import/export route-policy <name>", and "import/export route-target <community> stitching" for vrf.

--- lib/cisco_node_utils/vrf_af.rb
+++ lib/cisco_node_utils/vrf_af.rb
@@ -83,6 +83,40 @@ module Cisco
     #                      PROPERTIES                      #
     ########################################################
 
+    def route_policy_export
+      config_get('vrf', 'route_policy_export', @get_args)
+    end
+
+    def route_policy_export=(name)
+      if platform == :nexus
+        fail Cisco::UnsupportedError.new('vrf_af', route_policy_export)
+      end
+      state = name ? '' : 'no'
+      set_args_keys(state: state, policy_name: name)
+      config_set('vrf', 'route_policy_export', @set_args)
+    end
+
+    def default_route_policy_export
+      config_get_default('vrf', 'route_policy_export')
+    end
+
+    def route_policy_import
+      config_get('vrf', 'route_policy_import', @get_args)
+    end
+
+    def route_policy_import=(name)
+      if platform == :nexus
+        fail Cisco::UnsupportedError.new('vrf_af', route_policy_import)
+      end
+      state = name ? '' : 'no'
+      set_args_keys(state: state, policy_name: name)
+      config_set('vrf', 'route_policy_import', @set_args)
+    end
+
+    def default_route_policy_import
+      config_get_default('vrf', 'route_policy_import')
+    end
+
     def route_target_both_auto
       config_get('vrf', 'route_target_both_auto', @get_args)
     end
@@ -142,6 +176,21 @@ module Cisco
     end
 
     # --------------------------
+    def route_target_export_stitching
+      cmds = config_get('vrf', 'route_target_export_stitching', @get_args)
+      cmds.nil? ? nil : cmds.sort
+    end
+
+    def route_target_export_stitching=(should)
+      route_target_delta(should, route_target_export_stitching,
+                         'route_target_export_stitching')
+    end
+
+    def default_route_target_export_stitching
+      config_get_default('vrf', 'route_target_export_stitching')
+    end
+
+    # --------------------------
     def route_target_import
       cmds = config_get('vrf', 'route_target_import', @get_args)
       cmds.nil? ? nil : cmds.sort
@@ -168,6 +217,21 @@ module Cisco
 
     def default_route_target_import_evpn
       config_get_default('vrf', 'route_target_import_evpn')
+    end
+
+    # --------------------------
+    def route_target_import_stitching
+      cmds = config_get('vrf', 'route_target_import_stitching', @get_args)
+      cmds.nil? ? nil : cmds.sort
+    end
+
+    def route_target_import_stitching=(should)
+      route_target_delta(should, route_target_import_stitching,
+                         'route_target_import_stitching')
+    end
+
+    def default_route_target_import_stitching
+      config_get_default('vrf', 'route_target_import_stitching')
     end
 
     # --------------------------
