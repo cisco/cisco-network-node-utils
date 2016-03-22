91eaf1c842d130313446c220c3c4f512583f4526
sushirol <sushirol@cisco.com>
2016-03-22 20:02:07 +0000

    Install and uninstall cisco and third party rpm packages.

--- lib/cisco_node_utils/yum.rb
+++ lib/cisco_node_utils/yum.rb
@@ -20,57 +20,43 @@
 require_relative 'node_util'
 
 module Cisco
-  # This Yum class provides cisco package management functions.
+  # This Yum class provides cisco package management functions through nxapi.
   class Yum < NodeUtil
     EXEC_IN_DEFAULT_NS = 'ip netns exec default'
+    def self.decompose_name(file_name)
+      # ex: chef-12.0.0alpha.2+20150319.git.1.b6f-1.el5.x86_64.rpm
+      name_ver_arch_regex = /^([\w\-\+]+)-(\d+\..*)\.(\w{4,})(?:\.rpm)?$/
 
-    # This function accepts name of the rpm package and returns a match group.
-    def self.decompose_name(pkg)
-      file_name = pkg.strip.tr(':', '/').split('/').last
-      if platform == :nexus
-        # ex: chef-12.0.0alpha.2+20150319.git.1.b6f-1.el5.x86_64.rpm
-        name_ver_arch_regex = /^([\w\-\+]+)-(\d+\..*)\.(\w{4,})(?:\.rpm)?$/
-        # ex n9000_sample-1.0.0-7.0.3.x86_64.rpm
-        name_ver_arch_regex_nx = /^(.*)-([\d\.]+-[\d\.]+)\.(\w{4,})\.rpm$/
-        # ex: b+z-ip2.x64_64
-        name_arch_regex = /^([\w\-\+]+)\.(\w+)$/
+      # ex n9000_sample-1.0.0-7.0.3.x86_64.rpm
+      name_ver_arch_regex_nx = /^(.*)-([\d\.]+-[\d\.]+)\.(\w{4,})\.rpm$/
 
-        file_name.match(name_ver_arch_regex) ||
-          file_name.match(name_ver_arch_regex_nx) ||
-          file_name.match(name_arch_regex)
-      elsif platform == :ios_xr
-        # Match group when the rpm is for ios_xr :
-        #   1. package name, 2. package version, 3. os version, 4. platform
-        # ex xrv9k-k9sec-1.0.0.0-r600.x86_64.rpm-6.0.0
-        # xrv9k-k9sec-1.0.0.0-r61102I.x86_64.rpm-XR-DEV-16.02.22C
-        name_ver_arch_regex_xr = /^(.*\d.*)-([\d.]*)-(r\d+.*)\.(\w{4,}).rpm/
+      # ex: b+z-ip2.x64_64
+      name_arch_regex = /^([\w\-\+]+)\.(\w+)$/
 
-        file_name.match(name_ver_arch_regex_xr)
-      end
+      file_name.match(name_ver_arch_regex) ||
+        file_name.match(name_ver_arch_regex_nx) ||
+        file_name.match(name_arch_regex)
     end
 
     def self.validate(pkg)
-      pkg_info = Yum.decompose_name(pkg)
-      if pkg_info.nil?
+      if platform == :nexus
         file_name = pkg.strip.tr(':', '/').split('/').last
-        query_name = file_name
-      else
-        if pkg_info[3].nil?
-          query_name = pkg_info[1]
+        pkg_info = Yum.decompose_name(file_name)
+        if pkg_info.nil?
+          query_name = file_name
         else
-          if platform == :nexus
+          if pkg_info[3].nil?
+            query_name = pkg_info[1]
+          else
             query_name = "#{pkg_info[1]}.#{pkg_info[3]}"
-          elsif platform == :ios_xr
-            # ex query_name xrv9k-k9sec-1.0.0.0-r61102I.x86_64
-            query_name = \
-              "#{pkg_info[1]}-#{pkg_info[2]}-#{pkg_info[3]}.#{pkg_info[4]}"
           end
         end
+        should_ver = pkg_info[2] if pkg_info && pkg_info[3]
+      else
+        query_name = pkg
+        should_ver = pkg.match(/^.*\d.*-([\d.]*)-r\d+.*/)[1]
       end
-      should_ver = pkg_info[2] if pkg_info && pkg_info[3]
       ver = query(query_name)
-      Cisco::Logger.debug "Installed package version #{ver}, " \
-        "expected package version #{should_ver}"
       if ver.nil? || (!should_ver.nil? && should_ver != ver)
         fail 'Failed to install the requested rpm'
       end
@@ -89,76 +75,45 @@ module Cisco
       if platform == :nexus
         vrf = vrf.nil? ? detect_vrf : "vrf #{vrf}"
         config_set('yum', 'install', pkg, vrf)
-
-      elsif platform == :ios_xr
-        pkg_info = Yum.decompose_name(pkg)
-        if pkg_info
-          # ex pkg_name xrv9k-k9sec-1.0.0.0-r61102I
-          pkg_name = "#{pkg_info[1]}-#{pkg_info[2]}-#{pkg_info[3]}"
-          Cisco::Logger.debug "Installing package #{pkg_name}"
-          rc = `#{EXEC_IN_DEFAULT_NS} sdr_instcmd install activate pkg 0x0 \
-          #{pkg_name}`
-          Cisco::Logger.debug "install activate #{pkg_name} : #{rc}"
-          rc = `#{EXEC_IN_DEFAULT_NS} sdr_instcmd install commit sdr`
-          Cisco::Logger.debug "install commit sdr : #{rc}"
-        else
-          fail "Failed to parse name #{pkg}."
-        end
+      else
+        # ex pkg xrv9k-k9sec-1.0.0.0-r61102I
+        rc = `#{EXEC_IN_DEFAULT_NS} sdr_instcmd install activate pkg 0x0 \
+          #{pkg}`
+        Cisco::Logger.debug "install activate #{pkg} : #{rc}"
+        rc = `#{EXEC_IN_DEFAULT_NS} sdr_instcmd install commit sdr`
+        Cisco::Logger.debug "install commit : #{rc}"
       end
       # post-validation check to verify successful installation.
       validate(pkg)
     end
 
-    # returns version of the package if exists, otherwise returns nil
+    # returns version of package, or nil if package doesn't exist
     def self.query(pkg)
       fail TypeError unless pkg.is_a? String
       fail ArgumentError if pkg.empty?
       if platform == :nexus
         b = config_get('yum', 'query', pkg)
         fail "Multiple matching packages found for #{pkg}" if b && b.size > 1
-
-      elsif platform == :ios_xr
-        package_info = `#{EXEC_IN_DEFAULT_NS} sdr_instcmd show install \
-                     package #{pkg} none`
-
-        # sample output of show install package
-        #     Filename          : xrv9k-k9sec
-        #     Version           : 1.0.0.0
-        #     Parent Version    : (none)
-        #     Platform          : xrv9k
-        #     Package Type      : Package
-        #   Restart Type      : dependent
-        #   Install Method    : parallel
-        #   RPM Count         : 1
-        #
-        #   Package Contents     :
-        #     xrv9k-k9sec-1.0.0.0-r61102I.x86_64
-
-        ver_file_regex_xr = /^\s*Filename\s*:\s*(.*)\s*Version\s*:\s*(.*)/
-
-        package_info.match(ver_file_regex_xr)
-        filename = Regexp.last_match(1)
-        ver = Regexp.last_match(2)
-        is_active_regex = /^\s*#{filename}/
-
-        # In XR, even if package is inactive, 'show install package' returns
-        # information about the package. Hence we verify if the package is
-        # actived
-        is_active = `#{EXEC_IN_DEFAULT_NS} sdr_instcmd show install active`
-        if is_active =~ is_active_regex
-          Cisco::Logger.debug "Package #{filename} version #{ver} is present."
-          b = ["#{ver}"]
+        b.nil? ? nil : b.first
+      else
+        # Optionally strip the version from package name.
+        gr = pkg.match(/^([a-z0-9]+-[a-z0-9]+)(-)?/)
+        if gr.nil?
+          nil
+        else
+          active_pkgs_list = `#{EXEC_IN_DEFAULT_NS} sdr_instcmd show \
+          install active`
+          # Search for package in list of active packages.
+          active = active_pkgs_list.match(/^\s*#{gr[1]}-([\d\.]+)/)
+          active.nil? ? nil : active[1]
         end
       end
-      b.nil? ? nil : b.first
     end
 
     def self.remove(pkg)
-      fail TypeError unless pkg.is_a? String
-      fail ArgumentError if pkg.empty?
       if platform == :nexus
         config_set('yum', 'remove', pkg)
-      elsif platform == :ios_xr
+      else
         rc = \
           `#{EXEC_IN_DEFAULT_NS} sdr_instcmd install deactivate pkg 0x0 #{pkg}`
         Cisco::Logger.debug "install deactivate #{pkg} : #{rc}"
