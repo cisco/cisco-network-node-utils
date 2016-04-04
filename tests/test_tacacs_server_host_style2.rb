a9552a327df39f5e7d1044f8c99ff09f7bea31f8
Jonathan Tripathy <jt@puppetlabs.com>
2016-04-04 15:21:51 +0100

    Feedback update

--- tests/test_tacacs_server_host_style2.rb
+++ tests/test_tacacs_server_host_style2.rb
@@ -19,7 +19,7 @@ require_relative 'ciscotest'
 require_relative '../lib/cisco_node_utils/tacacs_server_host'
 
 # TestTacacsServerHost - Minitest for TacacsServerHost node utility.
-class TestTacacsServerHost < CiscoTestCase
+class TestTacacsServerHostStyle2 < CiscoTestCase
   @skip_unless_supported = 'tacacs_server_host'
 
   def setup
