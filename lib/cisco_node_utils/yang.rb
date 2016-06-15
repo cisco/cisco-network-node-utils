0b559d0860b6ff9d86511ca98b1ce5b0df655522
chburket <chburket@cisco.com>
2016-06-15 12:14:58 +0000

    Changes based on feedback

--- lib/cisco_node_utils/yang.rb
+++ lib/cisco_node_utils/yang.rb
@@ -27,41 +27,46 @@ module Cisco
       !yang || yang.empty?
     end
 
-    # Given a current and target YANG configuration, returns true if
+    # Given target and current YANG configurations, returns true if
     # the configurations are in-sync, relative to a "merge_config" action
-    def self.insync_for_merge?(target, current)
-      target_hash = self.empty?(target) ? {} : JSON.parse(target)
-      current_hash = self.empty?(current) ? {} : JSON.parse(current)
+    # @param should [String] the should configuration in YANG JASON format
+    # @param is [String] the is configuration in YANG JASON format
+    def self.insync_for_merge?(should, is)
+      should_hash = self.empty?(should) ? {} : JSON.parse(should)
+      is_hash = self.empty?(is) ? {} : JSON.parse(is)
 
-      !needs_something?(:merge, target_hash, current_hash)
+      !needs_something?(:merge, should_hash, is_hash)
     end
 
-    # Given a current and target YANG configuration, returns true if
+    # Given a is and should YANG configuration, returns true if
     # the configuration are in-sync, relative to a "replace_config" action
-    def self.insync_for_replace?(target, current)
-      target_hash = self.empty?(target) ? {} : JSON.parse(target)
-      current_hash = self.empty?(current) ? {} : JSON.parse(current)
+    # @param should [String] the should configuration in YANG JASON format
+    # @param is [String] the is configuration in YANG JASON format
+    def self.insync_for_replace?(should, is)
+      should_hash = self.empty?(should) ? {} : JSON.parse(should)
+      is_hash = self.empty?(is) ? {} : JSON.parse(is)
 
-      !needs_something?(:replace, target_hash, current_hash)
+      !needs_something?(:replace, should_hash, is_hash)
     end
+
 
     # usage:
-    #   needs_something?(op, target, run)
+    #   needs_something?(op, should, run)
     #
     #   op     - symbol - If value is not :replace, it's assumed to be :merge.
     #                     Indicates to the function whether to check for a
     #                     possible merge vs. replace
     #
-    #   target - JSON   - JSON tree representing target configuration
+    #   should - JSON   - JSON tree representing target configuration
     #
-    #   run    - JSON   - JSON tree representing target configuration
+    #   is     - JSON   - JSON tree representing current configuration
     #
     #
-    # Needs merge will determine if target and run differ
+    # Needs merge will determine if should and is differ
     # sufficiently to necessitate running the merge command.
     #
-    # The logic here amounts to determining if target is a subtree
-    # of run, with a tiny bit of domain trickiness surrounding
+    # The logic here amounts to determining if should is a subtree
+    # of is, with a tiny bit of domain trickiness surrounding
     # elements that are arrays that contain a single nil element
     # that is required for "creating" certain configuration elements.
     #
@@ -70,57 +75,57 @@ module Cisco
     # are organized with an order, the logic here ignores the order.
     # In fact, it specifically attempts to match assuming order
     # doesn't matter.  This is largely to allow users some freedom
-    # in specifying the config in the manifest.  The gRPC interface
+    # in specifying the target configuration.  The gRPC interface
     # doesn't seem to care about order.  If that changes, then so
     # should this code.
     #
     # Arrays and Hashes are compared by iterating over every element
-    # in target, and ensuring it is within run.
+    # in should, and ensuring it is within is.
     #
     # Leaves are directly compared for equality, excepting the
-    # condition that the target leaf is in fact an array with one
+    # condition that the should leaf is in fact an array with one
     # element that is nil.
     #
-    # Needs replace will determine if target and run differ
+    # Needs replace will determine if should and is differ
     # sufficiently to necessitate running the replace command.
     #
     # The logic is the same as merge, except when comparing
-    # hashes, if the run hash table has elements that are not
-    # in target, we ultimately indicate that replace is needed
-    def self.needs_something?(op, target, run)
-      !hash_equiv?(op, target, run)
+    # hashes, if the is hash table has elements that are not
+    # in should, we ultimately indicate that replace is needed
+    def self.needs_something?(op, should, is)
+      !hash_equiv?(op, should, is)
     end
 
     def self.nil_array(elt)
       elt.nil? || (elt.is_a?(Array) && elt.length == 1 && elt[0].nil?)
     end
 
-    def self.sub_elt(op, target, run)
-      if target.is_a?(Hash) && run.is_a?(Hash)
-        return self.hash_equiv?(op, target, run)
-      elsif target.is_a?(Array) && run.is_a?(Array)
-        return self.array_equiv?(op, target, run)
+    def self.sub_elt(op, should, is)
+      if should.is_a?(Hash) && is.is_a?(Hash)
+        return self.hash_equiv?(op, should, is)
+      elsif should.is_a?(Array) && is.is_a?(Array)
+        return self.array_equiv?(op, should, is)
       else
-        return !(target != run && !nil_array(target))
+        return !(should != is && !nil_array(should))
       end
     end
 
-    def self.array_equiv?(op, target, run)
-      n = target.length
+    def self.array_equiv?(op, should, is)
+      n = should.length
       loop = lambda do|i|
         if i == n
           if op == :replace
-            run.length == target.length
+            is.length == should.length
           else
             true
           end
         else
-          target_elt = target[i]
-          run_elt = run.find do |elt|
-            sub_elt(op, target_elt, elt)
+          should_elt = should[i]
+          is_elt = is.find do |elt|
+            sub_elt(op, should_elt, elt)
           end
-          if run_elt.nil? && !nil_array(target_elt)
-            target_elt.nil?
+          if is_elt.nil? && !nil_array(should_elt)
+            should_elt.nil?
           else
             loop.call(i + 1)
           end
@@ -129,24 +134,24 @@ module Cisco
       loop.call(0)
     end
 
-    def self.hash_equiv?(op, target, run)
-      keys = target.keys
+    def self.hash_equiv?(op, should, is)
+      keys = should.keys
       n = keys.length
       loop = lambda do|i|
         if i == n
           if op == :replace
-            run.keys.length == target.keys.length
+            is.keys.length == should.keys.length
           else
             true
           end
         else
           k = keys[i]
-          run_v = run[k]
-          target_v = target[k]
-          if run_v.nil? && !nil_array(target_v)
+          is_v = is[k]
+          should_v = should[k]
+          if is_v.nil? && !nil_array(should_v)
             false
           else
-            sub_elt(op, target_v, run_v) && loop.call(i + 1)
+            sub_elt(op, should_v, is_v) && loop.call(i + 1)
           end
         end
       end
