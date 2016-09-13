f662fe7d0a35d1686c61ebd56ed81a9eb7d1c676
saichint <saichint@cisco.com>
2016-09-13 13:46:22 -0700

    Dhcp Relay Global (#481)
    * removing vlan name negative tests
    
    * munging issue fixed
    
    * add dhcp relay global props
    
    * added a new prop and cleaned up names of props
    
    * add destroy method
    
    * doc
    
    * review comments
    
    * test code cleanup

---
+++ lib/cisco_node_utils/dhcp_relay_global.rb
@@ -0,0 +1,300 @@
+# September 2016, Sai Chintalapudi
+#
+# Copyright (c) 2016 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative 'node_util'
+
+module Cisco
+  # node_utils class for dhcp_relay_global
+  class DhcpRelayGlobal < NodeUtil
+    attr_reader :name
+
+    def initialize(instantiate=true)
+      Feature.dhcp_enable if instantiate
+      set_args_keys_default
+    end
+
+    def self.globals
+      { 'default' => DhcpRelayGlobal.new('default') }
+    end
+
+    # Helper method to delete @set_args hash keys
+    def set_args_keys_default
+      @set_args = {}
+      @get_args = @set_args
+    end
+
+    # rubocop:disable Style/AccessorMethodName
+    def set_args_keys(hash={})
+      set_args_keys_default
+      @set_args = @get_args.merge!(hash) unless hash.empty?
+    end
+
+    def destroy
+      return unless Feature.dhcp_enabled?
+      [:ipv4_information_option,
+       :ipv4_information_option_trust,
+       :ipv4_information_option_vpn,
+       :ipv4_information_trust_all,
+       :ipv4_relay,
+       :ipv4_smart_relay,
+       :ipv4_src_addr_hsrp,
+       :ipv4_src_intf,
+       :ipv4_sub_option_circuit_id_custom,
+       :ipv4_sub_option_circuit_id_string,
+       :ipv4_sub_option_cisco,
+       :ipv6_option_cisco,
+       :ipv6_option_vpn,
+       :ipv6_relay,
+       :ipv6_src_intf,
+      ].each do |prop|
+        send("#{prop}=", send("default_#{prop}")) if send prop
+      end
+      set_args_keys_default
+    end
+
+    ########################################################
+    #                      PROPERTIES                      #
+    ########################################################
+
+    def ipv4_information_option
+      config_get('dhcp_relay_global', 'ipv4_information_option')
+    end
+
+    def ipv4_information_option=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_information_option', @set_args)
+    end
+
+    def default_ipv4_information_option
+      config_get_default('dhcp_relay_global', 'ipv4_information_option')
+    end
+
+    def ipv4_information_option_trust
+      config_get('dhcp_relay_global', 'ipv4_information_option_trust')
+    end
+
+    def ipv4_information_option_trust=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_information_option_trust',
+                 @set_args)
+    end
+
+    def default_ipv4_information_option_trust
+      config_get_default('dhcp_relay_global', 'ipv4_information_option_trust')
+    end
+
+    def ipv4_information_option_vpn
+      config_get('dhcp_relay_global', 'ipv4_information_option_vpn')
+    end
+
+    def ipv4_information_option_vpn=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_information_option_vpn', @set_args)
+    end
+
+    def default_ipv4_information_option_vpn
+      config_get_default('dhcp_relay_global', 'ipv4_information_option_vpn')
+    end
+
+    def ipv4_information_trust_all
+      config_get('dhcp_relay_global', 'ipv4_information_trust_all')
+    end
+
+    def ipv4_information_trust_all=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_information_trust_all', @set_args)
+    end
+
+    def default_ipv4_information_trust_all
+      config_get_default('dhcp_relay_global', 'ipv4_information_trust_all')
+    end
+
+    def ipv4_relay
+      config_get('dhcp_relay_global', 'ipv4_relay')
+    end
+
+    def ipv4_relay=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_relay', @set_args)
+    end
+
+    def default_ipv4_relay
+      config_get_default('dhcp_relay_global', 'ipv4_relay')
+    end
+
+    def ipv4_smart_relay
+      config_get('dhcp_relay_global', 'ipv4_smart_relay')
+    end
+
+    def ipv4_smart_relay=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_smart_relay', @set_args)
+    end
+
+    def default_ipv4_smart_relay
+      config_get_default('dhcp_relay_global', 'ipv4_smart_relay')
+    end
+
+    def ipv4_src_addr_hsrp
+      config_get('dhcp_relay_global', 'ipv4_src_addr_hsrp')
+    end
+
+    def ipv4_src_addr_hsrp=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_src_addr_hsrp', @set_args)
+    end
+
+    def default_ipv4_src_addr_hsrp
+      config_get_default('dhcp_relay_global', 'ipv4_src_addr_hsrp')
+    end
+
+    def ipv4_src_intf
+      intf = config_get('dhcp_relay_global', 'ipv4_src_intf')
+      # Normalize by downcasing and removing white space
+      intf = intf.downcase.delete(' ') if intf
+      intf
+    end
+
+    def ipv4_src_intf=(val)
+      state = val == default_ipv4_src_intf ? 'no' : ''
+      intf = val == default_ipv4_src_intf ? '' : val
+      set_args_keys(state: state, intf: intf)
+      config_set('dhcp_relay_global', 'ipv4_src_intf', @set_args)
+    end
+
+    def default_ipv4_src_intf
+      config_get_default('dhcp_relay_global', 'ipv4_src_intf')
+    end
+
+    def ipv4_sub_option_circuit_id_custom
+      config_get('dhcp_relay_global', 'ipv4_sub_option_circuit_id_custom')
+    end
+
+    def ipv4_sub_option_circuit_id_custom=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_sub_option_circuit_id_custom',
+                 @set_args)
+    end
+
+    def default_ipv4_sub_option_circuit_id_custom
+      config_get_default('dhcp_relay_global',
+                         'ipv4_sub_option_circuit_id_custom')
+    end
+
+    def ipv4_sub_option_circuit_id_string
+      str = config_get('dhcp_relay_global', 'ipv4_sub_option_circuit_id_string')
+      # Normalize by removing white space
+      str.strip! if str
+      str
+    end
+
+    def ipv4_sub_option_circuit_id_string=(val)
+      state = val == default_ipv4_sub_option_circuit_id_string ? 'no' : ''
+      format = val == default_ipv4_sub_option_circuit_id_string ? '' : 'format'
+      word = val == default_ipv4_sub_option_circuit_id_string ? '' : val
+      set_args_keys(state: state, format: format, word: word)
+      config_set('dhcp_relay_global', 'ipv4_sub_option_circuit_id_string',
+                 @set_args)
+    end
+
+    def default_ipv4_sub_option_circuit_id_string
+      config_get_default('dhcp_relay_global',
+                         'ipv4_sub_option_circuit_id_string')
+    end
+
+    def ipv4_sub_option_cisco
+      config_get('dhcp_relay_global', 'ipv4_sub_option_cisco')
+    end
+
+    def ipv4_sub_option_cisco=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv4_sub_option_cisco', @set_args)
+    end
+
+    def default_ipv4_sub_option_cisco
+      config_get_default('dhcp_relay_global', 'ipv4_sub_option_cisco')
+    end
+
+    def ipv6_option_cisco
+      config_get('dhcp_relay_global', 'ipv6_option_cisco')
+    end
+
+    def ipv6_option_cisco=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv6_option_cisco', @set_args)
+    end
+
+    def default_ipv6_option_cisco
+      config_get_default('dhcp_relay_global', 'ipv6_option_cisco')
+    end
+
+    def ipv6_option_vpn
+      config_get('dhcp_relay_global', 'ipv6_option_vpn')
+    end
+
+    def ipv6_option_vpn=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv6_option_vpn', @set_args)
+    end
+
+    def default_ipv6_option_vpn
+      config_get_default('dhcp_relay_global', 'ipv6_option_vpn')
+    end
+
+    def ipv6_relay
+      config_get('dhcp_relay_global', 'ipv6_relay')
+    end
+
+    def ipv6_relay=(val)
+      state = val ? '' : 'no'
+      set_args_keys(state: state)
+      config_set('dhcp_relay_global', 'ipv6_relay', @set_args)
+    end
+
+    def default_ipv6_relay
+      config_get_default('dhcp_relay_global', 'ipv6_relay')
+    end
+
+    def ipv6_src_intf
+      intf = config_get('dhcp_relay_global', 'ipv6_src_intf')
+      # Normalize by downcasing and removing white space
+      intf = intf.downcase.delete(' ') if intf
+      intf
+    end
+
+    def ipv6_src_intf=(val)
+      state = val == default_ipv6_src_intf ? 'no' : ''
+      intf = val == default_ipv6_src_intf ? '' : val
+      set_args_keys(state: state, intf: intf)
+      config_set('dhcp_relay_global', 'ipv6_src_intf', @set_args)
+    end
+
+    def default_ipv6_src_intf
+      config_get_default('dhcp_relay_global', 'ipv6_src_intf')
+    end
+  end # class
+end # module
---
+++ tests/test_dhcp_relay_global.rb
@@ -0,0 +1,279 @@
+# Copyright (c) 2013-2016 Cisco and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative 'ciscotest'
+require_relative '../lib/cisco_node_utils/dhcp_relay_global'
+
+include Cisco
+# TestDhcpRelayGlobal - Minitest for general functionality
+# of the DhcpRelayGlobal class.
+class TestDhcpRelayGlobal < CiscoTestCase
+  @skip_unless_supported = 'dhcp_relay_global'
+  DEFAULT_NAME = 'default'
+
+  # Tests
+
+  def setup
+    super
+    config_no_warn('no feature dhcp')
+  end
+
+  def teardown
+    config_no_warn('no feature dhcp') if first_or_last_teardown
+    super
+  end
+
+  def create_dhcp_relay_global
+    DhcpRelayGlobal.new
+  end
+
+  def test_destroy
+    drg = create_dhcp_relay_global
+    assert_equal(true, Feature.dhcp_enabled?)
+
+    drg.destroy
+    [:ipv4_information_option,
+     :ipv4_information_option_trust,
+     :ipv4_information_option_vpn,
+     :ipv4_information_trust_all,
+     :ipv4_relay,
+     :ipv4_smart_relay,
+     :ipv4_src_addr_hsrp,
+     :ipv4_src_intf,
+     :ipv4_sub_option_circuit_id_custom,
+     :ipv4_sub_option_circuit_id_string,
+     :ipv4_sub_option_cisco,
+     :ipv6_option_cisco,
+     :ipv6_option_vpn,
+     :ipv6_relay,
+     :ipv6_src_intf,
+    ].each do |prop|
+      assert_equal(drg.send("default_#{prop}"), drg.send("#{prop}")) if
+        drg.send("#{prop}")
+    end
+  end
+
+  def test_ipv4_information_option
+    drg = create_dhcp_relay_global
+    assert_equal(drg.default_ipv4_information_option, drg.ipv4_information_option)
+    drg.ipv4_information_option = true
+    assert_equal(true, drg.ipv4_information_option)
+    drg.ipv4_information_option = drg.default_ipv4_information_option
+    assert_equal(drg.default_ipv4_information_option, drg.ipv4_information_option)
+  end
+
+  def test_ipv4_information_option_trust
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global',
+                                   'ipv4_information_option_trust')
+      assert_nil(drg.ipv4_information_option_trust)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv4_information_option_trust = true
+      end
+      return
+    end
+    assert_equal(drg.default_ipv4_information_option_trust,
+                 drg.ipv4_information_option_trust)
+    drg.ipv4_information_option_trust = true
+    assert_equal(true, drg.ipv4_information_option_trust)
+    drg.ipv4_information_option_trust = drg.default_ipv4_information_option_trust
+    assert_equal(drg.default_ipv4_information_option_trust,
+                 drg.ipv4_information_option_trust)
+  end
+
+  def test_ipv4_information_option_vpn
+    drg = create_dhcp_relay_global
+    assert_equal(drg.default_ipv4_information_option_vpn,
+                 drg.ipv4_information_option_vpn)
+    drg.ipv4_information_option_vpn = true
+    assert_equal(true, drg.ipv4_information_option_vpn)
+    drg.ipv4_information_option_vpn = drg.default_ipv4_information_option_vpn
+    assert_equal(drg.default_ipv4_information_option_vpn,
+                 drg.ipv4_information_option_vpn)
+  end
+
+  def test_ipv4_information_trust_all
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global',
+                                   'ipv4_information_trust_all')
+      assert_nil(drg.ipv4_information_trust_all)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv4_information_trust_all = true
+      end
+      return
+    end
+    assert_equal(drg.default_ipv4_information_trust_all,
+                 drg.ipv4_information_trust_all)
+    drg.ipv4_information_trust_all = true
+    assert_equal(true, drg.ipv4_information_trust_all)
+    drg.ipv4_information_trust_all = drg.default_ipv4_information_trust_all
+    assert_equal(drg.default_ipv4_information_trust_all,
+                 drg.ipv4_information_trust_all)
+  end
+
+  def test_ipv4_relay
+    drg = create_dhcp_relay_global
+    assert_equal(drg.default_ipv4_relay, drg.ipv4_relay)
+    drg.ipv4_relay = true
+    assert_equal(true, drg.ipv4_relay)
+    drg.ipv4_relay = false
+    assert_equal(false, drg.ipv4_relay)
+    drg.ipv4_relay = drg.default_ipv4_relay
+    assert_equal(drg.default_ipv4_relay, drg.ipv4_relay)
+  end
+
+  def test_ipv4_smart_relay
+    drg = create_dhcp_relay_global
+    assert_equal(drg.default_ipv4_smart_relay, drg.ipv4_smart_relay)
+    drg.ipv4_smart_relay = true
+    assert_equal(true, drg.ipv4_smart_relay)
+    drg.ipv4_smart_relay = drg.default_ipv4_smart_relay
+    assert_equal(drg.default_ipv4_smart_relay, drg.ipv4_smart_relay)
+  end
+
+  def test_ipv4_src_addr_hsrp
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global',
+                                   'ipv4_src_addr_hsrp')
+      assert_nil(drg.ipv4_src_addr_hsrp)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv4_src_addr_hsrp = true
+      end
+      return
+    end
+    assert_equal(drg.default_ipv4_src_addr_hsrp,
+                 drg.ipv4_src_addr_hsrp)
+    drg.ipv4_src_addr_hsrp = true
+    assert_equal(true, drg.ipv4_src_addr_hsrp)
+    drg.ipv4_src_addr_hsrp = drg.default_ipv4_src_addr_hsrp
+    assert_equal(drg.default_ipv4_src_addr_hsrp,
+                 drg.ipv4_src_addr_hsrp)
+  end
+
+  def test_ipv4_src_intf
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global', 'ipv4_src_intf')
+      assert_nil(drg.ipv4_src_intf)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv4_src_intf = 'port-channel200'
+      end
+      return
+    end
+    assert_equal(drg.default_ipv4_src_intf, drg.ipv4_src_intf)
+    drg.ipv4_src_intf = 'port-channel200'
+    assert_equal('port-channel200', drg.ipv4_src_intf)
+    drg.ipv4_src_intf = drg.default_ipv4_src_intf
+    assert_equal(drg.default_ipv4_src_intf, drg.ipv4_src_intf)
+  end
+
+  def test_ipv4_sub_option_circuit_id_custom
+    skip_nexus_i2_image?
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global',
+                                   'ipv4_sub_option_circuit_id_custom')
+      assert_nil(drg.ipv4_sub_option_circuit_id_custom)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv4_sub_option_circuit_id_custom = true
+      end
+      return
+    end
+    assert_equal(drg.default_ipv4_sub_option_circuit_id_custom,
+                 drg.ipv4_sub_option_circuit_id_custom)
+    drg.ipv4_sub_option_circuit_id_custom = true
+    assert_equal(true, drg.ipv4_sub_option_circuit_id_custom)
+    drg.ipv4_sub_option_circuit_id_custom = drg.default_ipv4_sub_option_circuit_id_custom
+    assert_equal(drg.default_ipv4_sub_option_circuit_id_custom,
+                 drg.ipv4_sub_option_circuit_id_custom)
+  end
+
+  def test_ipv4_sub_option_circuit_id_string
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global', 'ipv4_sub_option_circuit_id_string')
+      assert_nil(drg.ipv4_sub_option_circuit_id_string)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv4_sub_option_circuit_id_string = '%p%p'
+      end
+      return
+    end
+    assert_equal(drg.default_ipv4_sub_option_circuit_id_string, drg.ipv4_sub_option_circuit_id_string)
+    drg.ipv4_sub_option_circuit_id_string = '%p%p'
+    assert_equal('%p%p', drg.ipv4_sub_option_circuit_id_string)
+    drg.ipv4_sub_option_circuit_id_string = drg.default_ipv4_sub_option_circuit_id_string
+    assert_equal(drg.default_ipv4_sub_option_circuit_id_string, drg.ipv4_sub_option_circuit_id_string)
+  end
+
+  def test_ipv4_sub_option_cisco
+    drg = create_dhcp_relay_global
+    assert_equal(drg.default_ipv4_sub_option_cisco, drg.ipv4_sub_option_cisco)
+    drg.ipv4_sub_option_cisco = true
+    assert_equal(true, drg.ipv4_sub_option_cisco)
+    drg.ipv4_sub_option_cisco = drg.default_ipv4_sub_option_cisco
+    assert_equal(drg.default_ipv4_sub_option_cisco, drg.ipv4_sub_option_cisco)
+  end
+
+  def test_ipv6_option_cisco
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global',
+                                   'ipv6_option_cisco')
+      assert_nil(drg.ipv6_option_cisco)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv6_option_cisco = true
+      end
+      return
+    end
+    assert_equal(drg.default_ipv6_option_cisco,
+                 drg.ipv6_option_cisco)
+    drg.ipv6_option_cisco = true
+    assert_equal(true, drg.ipv6_option_cisco)
+    drg.ipv6_option_cisco = drg.default_ipv6_option_cisco
+    assert_equal(drg.default_ipv6_option_cisco,
+                 drg.ipv6_option_cisco)
+  end
+
+  def test_ipv6_option_vpn
+    drg = create_dhcp_relay_global
+    assert_equal(drg.default_ipv6_option_vpn, drg.ipv6_option_vpn)
+    drg.ipv6_option_vpn = true
+    assert_equal(true, drg.ipv6_option_vpn)
+    drg.ipv6_option_vpn = drg.default_ipv6_option_vpn
+    assert_equal(drg.default_ipv6_option_vpn, drg.ipv6_option_vpn)
+  end
+
+  def test_ipv6_relay
+    drg = create_dhcp_relay_global
+    assert_equal(drg.default_ipv6_relay, drg.ipv6_relay)
+    drg.ipv6_relay = true
+    assert_equal(true, drg.ipv6_relay)
+    drg.ipv6_relay = false
+    assert_equal(false, drg.ipv6_relay)
+    drg.ipv6_relay = drg.default_ipv6_relay
+    assert_equal(drg.default_ipv6_relay, drg.ipv6_relay)
+  end
+
+  def test_ipv6_src_intf
+    drg = create_dhcp_relay_global
+    if validate_property_excluded?('dhcp_relay_global', 'ipv6_src_intf')
+      assert_nil(drg.ipv6_src_intf)
+      assert_raises(Cisco::UnsupportedError) do
+        drg.ipv6_src_intf = 'loopback2'
+      end
+      return
+    end
+    assert_equal(drg.default_ipv6_src_intf, drg.ipv6_src_intf)
+    drg.ipv6_src_intf = 'loopback2 '
+    assert_equal('loopback2', drg.ipv6_src_intf)
+    drg.ipv6_src_intf = drg.default_ipv6_src_intf
+    assert_equal(drg.default_ipv6_src_intf, drg.ipv6_src_intf)
+  end
+end
