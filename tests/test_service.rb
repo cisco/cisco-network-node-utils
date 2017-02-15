cf28433df8387e1a7ffab1a9f131ef186d506192
Rahul Shenoy <rahushen@cisco.com>
2017-02-15 03:43:08 -0500

    Reworked some TCs + Add new TCs

--- tests/test_service.rb
+++ tests/test_service.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Cisco and/or its affiliates.
+# Copyright (c) 2016-2017 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,39 +14,77 @@
 
 require_relative 'ciscotest'
 require_relative '../lib/cisco_node_utils/service'
-require 'pp'
 
 include Cisco
 
 # TestService - Minitest for feature Service node Utility class
 class TestService < CiscoTestCase
   @skip_unless_supported = 'service'
+
+  def preconfig_service_info
+    path = File.expand_path('../service_info.yaml', __FILE__)
+    skip('Cannot find tests/service_info.yaml') unless File.file?(path)
+    info = YAML.load(File.read(path))
+    valid_info?(info)
+    info
+  end
+
+  def valid_info?(info)
+    skip('tests/service_info.yaml file is empty') unless info
+    msg = 'Missing key in tests/service_info.yaml'
+    %w(install_image install_media).each do |key|
+      skip("#{msg}: #{key}") if info[key].nil?
+    end
+  end
+
   ###################
   # Service tests   #
   ###################
 
   def test_clear_status
     Service.clear_status
-    assert_equal(Service.upgraded?, false)
+    refute(Service.upgraded?)
   end
 
   def test_delete
-    Service.delete('nxos.7.0.3.I5.0.231.CSCvb69953.bin')
+    shell_command('touch /bootflash/foobar')
+    Service.delete('foobar')
+    assert_raises(CliError) do
+      Service.delete('foobar')
+    end
   end
 
-  def test_delete_boot
-    Service.delete_boot
+  def test_delete_negative
+    assert_raises(CliError) do
+      # Delete a file that doesn't exist
+      Service.delete('foobar')
+    end
+    assert_raises(CliError) do
+      # Delete a file that doesn't exist
+      Service.delete('foobar', 'logflash:')
+    end
   end
+
+  # def test_delete_boot
+  #   Service.delete_boot
+  # end
 
   def test_image_version
-    assert_equal(Service.image_version('nxos.7.0.3.I2.1.bin'), '7.0(3)I2(1)')
+    version = Service.image_version
+    assert_match(/^\d.\d\(\d\)\S+\(\d+\)$/, version)
   end
 
   def test_upgrade
-    sleep 30
-    Service.upgrade('nxos.7.0.3.I2.5.bin')
-    # Wait for device to reboot
-    sleep 250
-    assert_equal(Service.upgraded?, true)
+    image_info = preconfig_service_info
+    Service.upgrade(image_info['install_image'], image_info['install_media'])
+    # Wait 15 seconds for device to reboot
+    sleep 15
+    begin
+      assert(Service.upgraded?)
+    rescue
+      tries ||= 1
+      retry unless (tries += 1) > 3
+      raise
+    end
   end
 end
