f8acbbb38f9b75011e7cfbe2c4d3bde8b6732e4b
rahushen <rahul.shenoy.86@gmail.com>
2017-03-04 16:51:53 -0500

    Upgrade services (#524)
    * Initial upgrade services
    
    * Remove white space
    
    * Allows tokens to be passed to get_command
    
    * Add tests and procs for install all nxos
    
    * Update DOC to highlight that get_command can be tokenized
    
    * Add YAML to specify params for upgrade
    
    * Adding error handling and cleaning up code
    
    * Reworked some TCs + Add new TCs
    
    * Handle case when upgrade is run with booted image
    
    * Add test_upgrade_boot_image
    
    * Add Service to CHANGELOG.md
    
    * Add TODO to upgrade scenario
    
    * Update CHANGELOG.md
    
    * Add command for upgrade_force
    
    * Update upgrade to handle delete_boot and force upgrade
    
    * Fix test_image_version
    
    * Add is_box_online
    
    * Enhancements to service.rb
    
    Function box_online? added.
    Modified upgraded? to check for config ready.
    Add version check to 'upgrade'.
    Handle 500 - Backend processing Error
    
    * Enchancements added to test_service.rb
    
    Added test for box_online?
    Modified calls to Service.upgrade to include version
    Increases retry to 5 in test_upgrade
    Made test_upgrade_boot_image skippable in absence of yaml file
    
    * Renaming files from service to upgrade
    
    * Updating references of service to upgrade
    
    * Renaming Service to Upgrade
    
    * Updating CHANGELOG.md
    
    * Incorporated @mikewiebe PR comments.
    
    - updated references of 'media' to 'uri'
    - fail upgrade if uri is not bootflash
    - replace shell_command with config to handle OAC
    
    * Fix Rubocop error

--- tests/test_upgrade.rb
+++ tests/test_upgrade.rb
@@ -13,88 +13,94 @@
 # limitations under the License.
 
 require_relative 'ciscotest'
-require_relative '../lib/cisco_node_utils/service'
+require_relative '../lib/cisco_node_utils/upgrade'
 
 include Cisco
 
-# TestService - Minitest for feature Service node Utility class
-class TestService < CiscoTestCase
-  @skip_unless_supported = 'service'
+# TestUpgrade - Minitest for router Upgrade node Utility class
+class TestUpgrade < CiscoTestCase
+  @skip_unless_supported = 'upgrade'
 
-  def preconfig_service_info
-    path = File.expand_path('../service_info.yaml', __FILE__)
-    skip('Cannot find tests/service_info.yaml') unless File.file?(path)
+  def preconfig_upgrade_info
+    path = File.expand_path('../upgrade_info.yaml', __FILE__)
+    skip('Cannot find tests/upgrade_info.yaml') unless File.file?(path)
     info = YAML.load(File.read(path))
     valid_info?(info)
     info
   end
 
   def valid_info?(info)
-    skip('tests/service_info.yaml file is empty') unless info
-    msg = 'Missing key in tests/service_info.yaml'
-    %w(install_image install_media).each do |key|
+    skip('tests/upgrade_info.yaml file is empty') unless info
+    msg = 'Missing key in tests/upgrade_info.yaml'
+    %w(install_image install_uri).each do |key|
       skip("#{msg}: #{key}") if info[key].nil?
     end
   end
 
   ###################
-  # Service tests   #
+  # Upgrade tests   #
   ###################
 
   def test_clear_status
-    Service.clear_status
-    refute(Service.upgraded?)
+    Upgrade.clear_status
+    refute(Upgrade.upgraded?)
   end
 
   def test_delete
-    shell_command('touch /bootflash/foobar')
-    Service.delete('foobar')
+    config('show version > bootflash:foobar')
+    Upgrade.delete('foobar')
     assert_raises(CliError) do
-      Service.delete('foobar')
+      Upgrade.delete('foobar')
     end
   end
 
   def test_delete_negative
     assert_raises(CliError) do
       # Delete a file that doesn't exist
-      Service.delete('foobar')
+      Upgrade.delete('foobar')
     end
     assert_raises(CliError) do
       # Delete a file that doesn't exist
-      Service.delete('foobar', 'logflash:')
+      Upgrade.delete('foobar', 'logflash:')
     end
   end
 
   # def test_delete_boot
-  #   Service.delete_boot
+  #   Upgrade.delete_boot
   # end
 
   def test_image_version
-    version = Service.image_version
-    assert_match(/^\d.\d\(\d\)\S+\(\d+\)$/, version)
+    version = Upgrade.image_version
+    assert_match(/^\d.\d\(\d\)\S+\(\S+\)$/, version)
+  end
+
+  def test_box_online
+    assert(Upgrade.box_online?)
   end
 
   def test_upgrade
-    image_info = preconfig_service_info
-    Service.upgrade(image_info['install_image'], image_info['install_media'])
+    image_info = preconfig_upgrade_info
+    version = Upgrade.image_version(image_info['install_image'], image_info['install_uri'])
+    Upgrade.upgrade(version, image_info['install_image'], image_info['install_uri'])
     # Wait 15 seconds for device to start rebooting
     # TODO : Consider getting the sleep value dynamically
     sleep 15
     begin
-      assert(Service.upgraded?)
+      assert(Upgrade.upgraded?)
     rescue
       tries ||= 1
-      retry unless (tries += 1) > 3
+      retry unless (tries += 1) > 5
       raise
     end
   end
 
   def test_upgrade_boot_image
+    preconfig_upgrade_info
     image_uri = node.config_get('show_version', 'system_image')
     image = image_uri.split('/')[-1]
-    media = image_uri.split('/')[0]
-    skip('Boot image not on bootflash:') unless media == 'bootflash:'
-    Service.upgrade(image, media)
-    assert(Service.upgraded?)
+    uri = image_uri.split('/')[0]
+    skip('Boot image not on bootflash:') unless uri == 'bootflash:'
+    Upgrade.upgrade(Upgrade.image_version, image, uri)
+    assert(Upgrade.upgraded?)
   end
 end
