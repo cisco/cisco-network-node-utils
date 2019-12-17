6d3b484e8a7b21da23827d0c638a38d1a506215f
Chris Van Heuveln <cvanheuv@cisco.com>
2019-12-17 16:27:07 -0500

    Better vdc errmsg

--- lib/cisco_node_utils/vdc.rb
+++ lib/cisco_node_utils/vdc.rb
@@ -46,7 +46,7 @@ module Cisco
 
     def self.default_vdc_name
       vdc = config_get('vdc', 'default_vdc_name')
-      fail RuntimeError if vdc.nil?
+      fail RuntimeError , 'default vdc not found, platform may not support vdc' if vdc.nil?
       vdc
     end
 
